6ce256b875894fe86f503829cfd9fec31963dae7
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2024-12-12 18:00:27 +0100

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ const client = generateClient<Schema>();
 
 function App() {
   //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [query, setQuery] = useState<string>("");
-  const [resultsList, setResultsList] = useState<string>("");
-  const [resultImage] = useState<string>("");
+  // const [query, setQuery] = useState<string>("");
+  // const [resultsList, setResultsList] = useState<string>("");
+  // const [resultImage] = useState<string>("");
 
-  const { signOut } = useAuthenticator();
+  // const { signOut } = useAuthenticator();
 
   // useEffect(() => {
   //   client.models.Todo.observeQuery().subscribe({
@@ -26,12 +26,12 @@ function App() {
   //   client.models.Todo.create({ content: window.prompt("Todo content") });
   // }
 
-  async function createModel()
-  {
-    var result = await client.queries.submitQuery({ query: query });
-    alert("query submitted to backend: "+JSON.stringify(result));
-    setResultsList(JSON.stringify(result));
-  }
+  // async function createModel()
+  // {
+  //   var result = await client.queries.submitQuery({ query: query });
+  //   alert("query submitted to backend: "+JSON.stringify(result));
+  //   setResultsList(JSON.stringify(result));
+  // }
 
   return (
     <BrowserRouter>
@@ -62,41 +62,41 @@ function App() {
   //   </main>;
   // }
 
-  async function claimPatreonBenefits() {
-    // send get request to patreon api, example: GET www.patreon.com/oauth2/authorize
-    // ?response_type=code
-    // &client_id=<your client id>
-    // &redirect_uri=<one of your redirect_uris that you provided in step 1>
-    // &scope=<optional list of requested scopes>
-    // &state=<optional string></optional>
-    // implement this web request in typescript
-    //var result = await client.queries.claimPatreonBenefits();
-    //alert("query submitted to backend: "+JSON.stringify(result));
+  // async function claimPatreonBenefits() {
+  //   // send get request to patreon api, example: GET www.patreon.com/oauth2/authorize
+  //   // ?response_type=code
+  //   // &client_id=<your client id>
+  //   // &redirect_uri=<one of your redirect_uris that you provided in step 1>
+  //   // &scope=<optional list of requested scopes>
+  //   // &state=<optional string></optional>
+  //   // implement this web request in typescript
+  //   //var result = await client.queries.claimPatreonBenefits();
+  //   //alert("query submitted to backend: "+JSON.stringify(result));
   
-    window.location.href="https://www.patreon.com/oauth2/authorize?response_type=code&client_id=MvmE1rACZyeWKOpBhynZjK0m4MWkJOKw_SXfo2CZfBFm7N2q9x7_ROzg8ZpAblZ0&redirect_uri=http://localhost:5173/patreon-connection";
-  }
+  //   window.location.href="https://www.patreon.com/oauth2/authorize?response_type=code&client_id=MvmE1rACZyeWKOpBhynZjK0m4MWkJOKw_SXfo2CZfBFm7N2q9x7_ROzg8ZpAblZ0&redirect_uri=http://localhost:5173/patreon-connection";
+  // }
 
-  function chat_simple() {
-    return <main>
-      <h1>What are you looking for?</h1>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            createModel();
-          }
-        }}
-      />
-      <button onClick={createModel}>Create</button>
-      <button onClick={signOut}>Sign out</button>
-      <button onClick={claimPatreonBenefits}>Claim Your Patreon Benefits</button>
+  // function chat_simple() {
+  //   return <main>
+  //     <h1>What are you looking for?</h1>
+  //     <input
+  //       type="text"
+  //       value={query}
+  //       onChange={(e) => setQuery(e.target.value)}
+  //       onKeyPress={(e) => {
+  //         if (e.key === "Enter") {
+  //           createModel();
+  //         }
+  //       }}
+  //     />
+  //     <button onClick={createModel}>Create</button>
+  //     <button onClick={signOut}>Sign out</button>
+  //     <button onClick={claimPatreonBenefits}>Claim Your Patreon Benefits</button>
 
-      <div>{resultsList}</div>
-      <img src={resultImage}></img>
-    </main>
-  }
+  //     <div>{resultsList}</div>
+  //     <img src={resultImage}></img>
+  //   </main>
+  // }
 }
 
 export default App;

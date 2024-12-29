c7e909e99b4484ac4b12fa4a1f225c159c52f35f
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2024-12-29 09:14:00 +0100

    make chat the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./Pages/Layout";
-import Home from "./Pages/Home";
+//import Home from "./Pages/Home";
 import Chat from "./Pages/Chat";
 import Profile from "./Pages/Profile";
 import ClaimPatreon from "./Pages/ClaimPatreon";
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Chat />} />
           <Route path="/chat/:chatId?" element={<Chat />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/claim-patreon" element={<ClaimPatreon />} />

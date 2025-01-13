358ece627e6ca56153a65b4af59ffd576f7d7d93
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-01-13 23:31:49 +0100

    customize signup emails

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,7 +9,19 @@ import { postAuthentication } from './post-authentication/resource';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: 'CODE',
+      verificationEmailSubject: 'Welcome to Chat3D! Here is your email verification code',
+      verificationEmailBody: (code) => `We're happy to have you!<br/><br/>Your verification code is: ${code()}<br/><br/>Best regards<br/>Chat3D Team`,
+      userInvitation: {
+        emailSubject: 'Welcome to Chat3D! Here is your temporary password',
+        emailBody: (user, code) => 
+          `We're happy to have you!<br/><br/> You can now login with username ${user()} and temporary password ${code()}<br/>
+          Please change your password after logging in.<br/><br/>
+          The link to the app is: ${process.env.GOOGLE_CALLBACK_URL}<br/><br/>
+          Best regards<br/>Chat3D Team`,
+      }
+    },
 
     externalProviders: {
       google: {

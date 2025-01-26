f76e0855d56e7b217b611582704bdf7d3d9bf4f4
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-01-26 20:18:52 +0100

    fix brain

--- src/Components/ChatContextComponent.tsx
+++ src/Components/ChatContextComponent.tsx
@@ -1,8 +1,9 @@
-import { Icon, Menu, MenuItem, Popup } from "semantic-ui-react";
+import { Confirm, Icon, Menu, MenuItem, Popup } from "semantic-ui-react";
 import { IChatMessage, Schema } from "../../amplify/data/resource";
 import { NavLink, useNavigate } from "react-router";
 import { generateClient } from "aws-amplify/api";
 import { list, remove } from "aws-amplify/storage";
+import { useState } from "react";
 const client = generateClient<Schema>();
 
 interface ChatContextComponentProps {
@@ -12,6 +13,47 @@ interface ChatContextComponentProps {
 
 const ChatContextComponent: React.FC<ChatContextComponentProps> = ({chatContext, onClick}) => {
     const navigate = useNavigate();
+    const [confirmDeleteOpen, setConfirmDeleteOpen] = useState(false);
+
+    async function onDeleteChatContext() {
+        console.log("trying to delete: "+chatContext.id);
+        // iterate over all chat items and delete them
+        const chatContextToDelete = await client.models.ChatContext.get({ id: chatContext.id });
+        if(chatContextToDelete.data)
+        {
+            const chatItemsForChatContext = await chatContextToDelete.data.chatItems();
+            chatItemsForChatContext.data.forEach(async (item) => {
+                console.log("deleting chat item: "+item.id);
+                // iterate over all messages in the chatItem and delete all scad and png files in the s3 bucket matching the message id
+                if(item.messages)
+                {
+                    if (typeof item.messages === 'string') {
+                        const messages: IChatMessage[] = item.messages ? JSON.parse(item.messages as string) : [];
+                        
+                        messages.forEach(async (message) => {
+                            if(message.itemType === "image")
+                            {
+                                const key = "modelcreator/"+message.id;
+
+                                var filesForKey = await list({path: key});
+                                //console.log("filesForKey: "+JSON.stringify(filesForKey));
+                                filesForKey.items.forEach(async (file) => {
+                                    //console.log("deleting file: "+file.path);
+                                    await remove({path: file.path});
+                                });
+                            }
+                        });
+                    }
+                }
+                await client.models.ChatItem.delete({ id: item.id });
+            });
+
+            console.log("deleting chat context: "+chatContext.id);
+            await client.models.ChatContext.delete({ id: chatContext.id });
+            navigate("/chat/new");
+        }
+    }
+
     return(
         <MenuItem as={NavLink}
         to={"/chat/"+chatContext.id}
@@ -31,51 +73,16 @@ const ChatContextComponent: React.FC<ChatContextComponentProps> = ({chatContext,
                             <Icon name="edit"/>
                             Rename
                         </MenuItem>
-                        <MenuItem onClick={async function (e) {
-                                e.preventDefault();
-                                console.log("trying to delete: "+chatContext.id);
-                                // iterate over all chat items and delete them
-                                const chatContextToDelete = await client.models.ChatContext.get({ id: chatContext.id });
-                                if(chatContextToDelete.data)
-                                {
-                                    const chatItemsForChatContext = await chatContextToDelete.data.chatItems();
-                                    chatItemsForChatContext.data.forEach(async (item) => {
-                                        console.log("deleting chat item: "+item.id);
-                                        // iterate over all messages in the chatItem and delete all scad and png files in the s3 bucket matching the message id
-                                        if(item.messages)
-                                        {
-                                            if (typeof item.messages === 'string') {
-                                                const messages: IChatMessage[] = item.messages ? JSON.parse(item.messages as string) : [];
-                                                
-                                                messages.forEach(async (message) => {
-                                                    if(message.itemType === "image")
-                                                    {
-                                                        const key = "modelcreator/"+message.id;
-
-                                                        var filesForKey = await list({path: key});
-                                                        //console.log("filesForKey: "+JSON.stringify(filesForKey));
-                                                        filesForKey.items.forEach(async (file) => {
-                                                            //console.log("deleting file: "+file.path);
-                                                            await remove({path: file.path});
-                                                        });
-                                                    }
-                                                });
-                                            }
-                                        }
-                                        await client.models.ChatItem.delete({ id: item.id });
-                                    });
-
-                                    console.log("deleting chat context: "+chatContext.id);
-                                    await client.models.ChatContext.delete({ id: chatContext.id });
-                                    navigate("/chat/new");
-                                }
-
-                                }}>
+                        <MenuItem onClick={()=>setConfirmDeleteOpen(true)}>
                             <Icon name="trash"/>
                             Delete
                         </MenuItem>
                     </Menu>
                 </Popup>
+                <Confirm 
+                    open={confirmDeleteOpen} 
+                    onCancel={() => setConfirmDeleteOpen(false)} 
+                    onConfirm={onDeleteChatContext} />
             </div>
         </div>   
         <div className="default-content overflow ellipsis">

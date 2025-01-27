39dfbda93b94cac08a7b5c9977cc7c014e07aaaa
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-01-27 17:32:48 +0100

    icon loading fixed?

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import 'semantic-ui-css/semantic.min.css';
-import 'semantic-ui-css/components/icon.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import WithResponsiveness from "./App.tsx";

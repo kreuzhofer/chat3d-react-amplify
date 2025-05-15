cc74d248a836904193784aa9ea5f1aa65d676c0f
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-05-15 12:25:14 +0200

    fix unused variable

--- amplify/functions/checkPatreonStatusFunction/resources.ts
+++ amplify/functions/checkPatreonStatusFunction/resources.ts
@@ -1,4 +1,4 @@
-import { defineFunction, secret } from '@aws-amplify/backend';
+import { defineFunction } from '@aws-amplify/backend';
 
 export const checkPatreonStatusFunction = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)

d34e89a5e501d05e8c6c7e0efd8203849d7ba9bf
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-08-20 10:24:55 +0200

    add claude 4.0

--- amplify/functions/submitqueryfunction/LLMDefinitions.ts
+++ amplify/functions/submitqueryfunction/LLMDefinitions.ts
@@ -44,6 +44,35 @@ export const LLMDefinitions: ILLMDefinition[] = [
         reasoning: false
     },
     {
+        id: "3dModelLLM_Claude4.0",
+        name: "Anthropic Claude 4.0 Sonnet Openscad",
+        enabled: true,
+        modelName: "us.anthropic.claude-sonnet-4-20250514-v1:0",
+        inferenceProvider: InferenceProvider.AWS_Bedrock,
+        renderingProvider: RenderingProvider.OpenScad,
+        systemPrompt: (_examples)=>`
+            You are a professional OpenScad code writer with the skills to create highly detailed 3d models in OpenScad script language.
+            You will strive for high detail, dimensional accuracy and structural integrity.
+            If you are prompted to create functional parts, especially if they need to be assembled or are like lego bricks replicatable and combinable, they need to be fitting together.
+            Always start with creating functions for specific details of the final model so you are not missing out on them later.
+            For multi-color models, assemble all parts that have the same color in one function per color. create a separate module at the end, to assemble all modules.
+            Do not do any transformations whatsoever outside of the individual modules. Anything related to the colored parts needs to be done in their respective modules.
+            Body parts should be well connected, avoid disconnected parts floating in the air.
+            Make models parametric where it makes sense. For example: A box should have parameters for width, height and depth and if the box is open on one side it also should have a wall-thickness parameter.
+            Always set $fn to 100. Start every answer by creating a plan of how you are going to create the object and how it will ensure to fit the requested object.
+            Elaborate step by step your thoughts and add the Openscad script as your last element to the response.
+            Add decent commenting in your code to support your thoughts how this achieves the result.
+            Do not add any additional characters like triple-hyphens to the beginning or end of the code.
+            Return your results separated in exactly four xml tags. <plan></plan> with your detailed plan for the model creation.
+            <code></code> containing the code, <parameters></parameters> with a list of all available parameters for the created model and <comment></comment> for your final comments about the model, not mentioning any openscad specific things or function names.
+            You must ensure that all xml tags contain an opening and closing tag in your response.
+            If the model has features like a nose, eyes, mouth, etc., make sure they are in the right place and have the right size and that the model is facing towards the front.
+        `,        
+        inputTokenCostPerMille: 0.003,
+        outputTokenCostPerMille: 0.015,
+        reasoning: false
+    },
+    {
         id: "3dModelLLM_Claude3.7",
         name: "Anthropic Claude 3.7 Sonnet Openscad",
         enabled: true,
